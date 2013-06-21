11dbc108d5de2bd411b9e4665c97965d659eee46
adeyemi ifaturoti <ifaturotiadeyemi@gmail.com>
2013-06-21 18:53:23 +0200

    Signed-off-by: adeyemi ifaturoti <ifaturotiadeyemi@gmail.com>

--- server.js
+++ server.js
@@ -43,8 +43,9 @@ String.prototype.contains = function(it) { return this.indexOf(it) != -1; };
 
 /*jslint nomen: true, regexp: true, unparam: true, stupid: true */
 /*global require, __dirname, unescape, console */
-console.log("the port here is>> "+port);
+console.log("\n\n\n\n\n >>>>>>>>>>>>>>>>>> the port here is>> "+port);
 (function (port) {
+	console.log("\n\n\n\n\n >>>>>>>>>>>>>>>>>> the port here is>> "+port);
     'use strict';
     var path = require('path'),
         fs = require('fs'),
